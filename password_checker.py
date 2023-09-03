6d9d7b78c0d4b776e5fac4163f56780d0781b3ed
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-03 20:56:45 +0100

    new

---
+++ password_checker.py
@@ -0,0 +1,23 @@
+# Ask the user to enter a password
+password = input("Enter a password: ")
+
+# Initialize variables to check criteria
+has_upper = False
+has_lower = False
+has_digit = False
+
+# Check password criteria
+if len(password) >= 8:
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+
+# Check if all criteria are met
+if has_upper and has_lower and has_digit:
+    print("Password is valid.")
+else:
+    print("Password is not valid. Make sure it has at least 8 characters, one uppercase letter, one lowercase letter, and one digit.")
