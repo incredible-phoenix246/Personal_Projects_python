f369d32b316bf3fcf6d8b6964093e23f64e1fe3c
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-03 23:11:16 +0100

    .

---
+++ skeleton_for_gui.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+
+layout = [[]]
+
+wimdows = sg.Windows('converter', layout)
+
+while True:
+    event, values = windows.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+windows.close()
