e730241149f4a982227e08d5055295865b50c808
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-03 21:57:36 +0100

    new_changes

--- password_checker.py
+++ password_checker.py
@@ -5,6 +5,10 @@ password = input("Enter a password: ")
 has_upper = False
 has_lower = False
 has_digit = False
+has_special = False
+
+# Define a set of special characters
+special_characters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\"
 
 # Check password criteria
 if len(password) >= 8:
@@ -15,9 +19,11 @@ if len(password) >= 8:
             has_lower = True
         elif char.isdigit():
             has_digit = True
+        elif char in special_characters:
+            has_special = True
 
 # Check if all criteria are met
-if has_upper and has_lower and has_digit:
+if has_upper and has_lower and has_digit and has_special:
     print("Password is valid.")
 else:
-    print("Password is not valid. Make sure it has at least 8 characters, one uppercase letter, one lowercase letter, and one digit.")
+    print("Password is not valid. Make sure it has at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character.")
